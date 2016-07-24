30a4bb5755db698fd7903be89aba25312c651cd7
ACharLuk <acharluk@gmail.com>
2016-07-24 13:15:25 +0200

    App existing error handling

--- .ccam/api/ccam.lua
+++ .ccam/api/ccam.lua
@@ -1,5 +1,9 @@
 function downloadApp(app_name)
 	-- Check if app already exists
+	if appExists(app_name) then
+		printError("Error: App already exists\nUse option 'update' to update the app")
+		return false
+	end
 
 	-- Download files
 
@@ -8,6 +12,10 @@ end
 
 function removeApp(app_name)
 	-- Check that app exists
+	if not appExists(app_name) then
+		printError("Error: App doesn't exist")
+		return false
+	end
 
 	-- Remove the app
 
@@ -15,12 +23,22 @@ end
 
 function updateApp(app_name)
 	-- Check that app exists
+	if not appExists(app_name) then
+		printError("Error: App doesn't exist")
+		return false
+	end
 
 	-- Update the app
 
 end
 
 function checkForUpdate(app_name)
+	-- Check that app exists
+	if not appExists(app_name) then
+		printError("Error: App doesn't exist")
+		return false
+	end
+
 	-- Check current version
 
 	-- Check remote version
@@ -30,7 +48,14 @@ function checkForUpdate(app_name)
 end
 
 function getAppVersion(app_name)
+	local app_json_file = fs.open(CCAM_CONF.APP_DIR .. app_name .. CCAM_CONF.APP_CONF, 'r')
 
+	-- Decode JSON
+
+	app_json_file.close()
+
+	-- Return version
+	
 end
 
 function appExists(app_name)
