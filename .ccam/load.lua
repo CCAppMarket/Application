266fc59e42d97ee40ea54964604f7e4a76fa9f51
ACharLuk <acharluk@gmail.com>
2016-07-29 01:53:35 +0200

    New library design

--- .ccam/load.lua
+++ .ccam/load.lua
@@ -5,20 +5,15 @@ local conf = loadfile("/.ccam/config.cfg")
 setfenv(conf, CCAM_CONF)
 conf()
 
---@Deprecated
---@TODO: New library desing
---[[ Load libraries into tables ]]--
+--[[ Load libraries ]]--
 -- List libraries
 local libraries = fs.list(CCAM_CONF.LIB_DIR)
 
+-- Load libraries into table
 for _, v in pairs(libraries) do
-	-- Remove extension for table name
-	local table_name = v:sub(1, v:find(".lua") - 1)
-
-	-- Load library into table
-	_G[table_name] = {}
-	local lib = loadfile(CCAM_CONF.LIB_DIR .. v)
-	setfenv(lib, setmetatable( _G[table_name], { __index = _G } ))
+	_G[v] = {}
+	local lib = loadfile(CCAM_CONF.LIB_DIR .. v .. CCAM_CONF.LIB_MAIN)
+	setfenv(lib, setmetatable( _G[v], { __index = _G } ))
 	lib()
 end
 
